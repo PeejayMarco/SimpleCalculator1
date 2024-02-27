3db1b1d4b09c1e17696e7c976f462bfcc03208a3
PeejayMarco <158016001+PeejayMarco@users.noreply.github.com>
2024-02-27 10:22:42 +0800

    Initial commit

---
+++ SimpleCalculator1/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace SimpleCalculator1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstNumberTextBox = new System.Windows.Forms.TextBox();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.secondNumberTextBox = new System.Windows.Forms.TextBox();
+            this.operationComboBox = new System.Windows.Forms.ComboBox();
+            this.computeResultButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // firstNumberTextBox
+            // 
+            this.firstNumberTextBox.Location = new System.Drawing.Point(99, 214);
+            this.firstNumberTextBox.Name = "firstNumberTextBox";
+            this.firstNumberTextBox.Size = new System.Drawing.Size(100, 20);
+            this.firstNumberTextBox.TabIndex = 0;
+            this.firstNumberTextBox.TextChanged += new System.EventHandler(this.firstNumberTextBox_TextChanged);
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(538, 215);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(100, 20);
+            this.resultTextBox.TabIndex = 1;
+            this.resultTextBox.TextChanged += new System.EventHandler(this.resultTextBox_TextChanged);
+            // 
+            // secondNumberTextBox
+            // 
+            this.secondNumberTextBox.Location = new System.Drawing.Point(295, 213);
+            this.secondNumberTextBox.Name = "secondNumberTextBox";
+            this.secondNumberTextBox.Size = new System.Drawing.Size(100, 20);
+            this.secondNumberTextBox.TabIndex = 2;
+            this.secondNumberTextBox.TextChanged += new System.EventHandler(this.secondNumberTextBox_TextChanged);
+            // 
+            // operationComboBox
+            // 
+            this.operationComboBox.FormattingEnabled = true;
+            this.operationComboBox.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.operationComboBox.Location = new System.Drawing.Point(215, 213);
+            this.operationComboBox.Name = "operationComboBox";
+            this.operationComboBox.Size = new System.Drawing.Size(63, 21);
+            this.operationComboBox.TabIndex = 3;
+            this.operationComboBox.SelectedIndexChanged += new System.EventHandler(this.operationComboBox_SelectedIndexChanged);
+            // 
+            // computeResultButton
+            // 
+            this.computeResultButton.Location = new System.Drawing.Point(411, 212);
+            this.computeResultButton.Name = "computeResultButton";
+            this.computeResultButton.Size = new System.Drawing.Size(103, 23);
+            this.computeResultButton.TabIndex = 4;
+            this.computeResultButton.Text = "Compute Result";
+            this.computeResultButton.UseVisualStyleBackColor = true;
+            this.computeResultButton.Click += new System.EventHandler(this.computeResultButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 182);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "1st Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(312, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "2nd Number";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(565, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.computeResultButton);
+            this.Controls.Add(this.operationComboBox);
+            this.Controls.Add(this.secondNumberTextBox);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.firstNumberTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox firstNumberTextBox;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.TextBox secondNumberTextBox;
+        private System.Windows.Forms.ComboBox operationComboBox;
+        private System.Windows.Forms.Button computeResultButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ SimpleCalculator1/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCalculator1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void resultTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void firstNumberTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void operationComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            if (double.TryParse(firstNumberTextBox.Text, out double firstNumber) &&
+                double.TryParse(secondNumberTextBox.Text, out double secondNumber))
+            {
+
+                string selectedOperator = operationComboBox.SelectedItem.ToString();
+
+
+                double result = 0;
+                switch (selectedOperator)
+                {
+                    case "+":
+                        result = firstNumber + secondNumber;
+                        break;
+                    case "-":
+                        result = firstNumber - secondNumber;
+                        break;
+                    case "*":
+                        result = firstNumber * secondNumber;
+                        break;
+                    case "/":
+                        if (secondNumber != 0)
+                            result = firstNumber / secondNumber;
+                        else
+                            MessageBox.Show("Cannot divide by zero!");
+                        break;
+                }
+
+
+                resultTextBox.Text = result.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid numeric values for operands.");
+            }
+        }
+
+        private void secondNumberTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void computeResultButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
